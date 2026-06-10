2ded7de0c5846e4b01bafce164b6ae800cb5bc64
Shin <penspulse@gmail.com>
2026-06-10 15:15:38 +0800

    add 4-2

--- src/features/ProbabilitySystem.ts
+++ src/features/ProbabilitySystem.ts
@@ -1,5 +1,18 @@
 export class ProbabilitySystem {
+  private readonly reels: Array<Array<string>> = [
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', 'Q', 'K'],
+    ['A', '10', 'J'],
+  ];
   spin(betLine: string): number {
-    return betLine === 'L1' ? 20 : 0;
+    const firstElementsSet = new Set<string>();
+
+    this.reels.forEach((reel) => {
+      firstElementsSet.add(reel[0]);
+    });
+
+    return firstElementsSet.size === 1 && betLine === 'L1' ? 20 : 0;
   }
 }
